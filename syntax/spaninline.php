a81e284ecf60ab7d2ded15dd31ed0e0ca6b75dbf
GHSRobert Ciang <Robertus0617@gmail.com>
2020-10-28 22:05:38 +0800

    Hack spaninline
    {[class[content]}

--- syntax/spaninline.php
+++ syntax/spaninline.php
@@ -12,9 +12,9 @@ require_once(dirname(__FILE__).'/span.php');
 
 class syntax_plugin_wrap_spaninline extends syntax_plugin_wrap_span {
 
-    protected $special_pattern = '<inline\b[^>\r\n]*?/>';
-    protected $entry_pattern   = '<inline\b.*?>(?=.*?</inline>)';
-    protected $exit_pattern    = '</inline>';
+    protected $special_pattern = '\{\[\b[^>\r\n]*?\]\}';
+    protected $entry_pattern   = '\{\[\b.*?\[(?=.*?\]\})';
+    protected $exit_pattern    = '\]\}';
 
 }
 
